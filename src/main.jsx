64e1ae6cd04aba5adfef8cffb7fcb9d4b87a5670
Nathan Begin <nathanbegin@hotmail.com>
2026-08-28 00:02:10 -0400

    Route /admin to sensor administration console

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // Inclus Tailwind
+import Admin from './Admin.jsx';
+import './index.css';
+
+const isAdminRoute = window.location.pathname === '/admin' ||
+  window.location.pathname.startsWith('/admin/');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdminRoute ? <Admin /> : <App />}
   </React.StrictMode>
 );
