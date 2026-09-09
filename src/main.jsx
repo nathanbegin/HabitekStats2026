3c847e86dd84c4461b50c0ae20929a3a037962ea
Nathan Begin <nathanbegin@hotmail.com>
2026-09-08 21:26:25 -0400

    Enable comparison chart controls and export fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Admin from './Admin.jsx';
+import './comparisonEnhancements.js';
 import './index.css';
 
 const isAdminRoute = window.location.pathname === '/admin' ||
